32e205e2f64ce80248e5bf84311162cb5aaa98d4
Rashad Guluzadeh <rashadquluzade@gmail.com>
2022-12-01 11:47:48 +0400

    redux configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./state/store"
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-)
+  </Provider>
+);
---
+++ src/state/reducers/reducer.js
@@ -0,0 +1,15 @@
+import { ActionTypes } from "../actions/action-types"
+
+const initialState = {
+
+
+
+}
+
+const reducer = (state=initialState, {type, payload}) => {
+    switch (type) {
+        default: return state
+    }
+}
+
+export default reducer
