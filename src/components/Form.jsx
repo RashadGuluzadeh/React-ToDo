2d485da5e3bc2c34f6da1cfc6204903fcd0a7442
Rashad Guluzadeh <rashadquluzade@gmail.com>
2022-12-02 13:07:56 +0400

    completed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -24,7 +24,7 @@ const Form = () => {
   return (
     <>
     <form onSubmit={handleSubmit} className="p-4 flex relative">
-      <h1 className="absolute top-[-35px] left-24 px-2 bg-white text-3xl text font-bold">To Do list</h1>
+      <h1 className="absolute top-[-35px] left-32 px-2 bg-white text-3xl text font-bold">To Do list</h1>
       <input
         type="text"
         className="border-2 border-black w-80 p-2 outline-none"
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,34 +3,35 @@ import { useSelector, useDispatch } from "react-redux";
 import { deleteTodo, removeTodo } from "../state/actions";
 import { RiDeleteBinLine } from "react-icons/ri";
 import { BiEdit } from "react-icons/bi";
+import { Scrollbar } from "react-scrollbars-custom";
 
 export const Todos = () => {
   const { todo } = useSelector((state) => state.data);
   const [empty, setEmpty] = useState(true);
-  const [edit, setEdit] = useState(false);
+  const [scroll, setScroll] = useState(false);
   useEffect(() => {
     if (todo.length !== 0) {
       setEmpty(false);
     } else {
       setEmpty(true);
     }
+    if (todo.length > 3) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
   });
   const dispatch = useDispatch();
   return (
-    <div className="flex items-center text-center justify-center flex-col gap-3">
+    <>
+    <div className={`flex items-center text-center justify-center flex-col gap-3 max-h-[168px] overflow-hidden ${scroll ?   "overflow-y-scroll overflow-x-hidden" : null} `}>
       {empty && <h3 className="flex justify-center">No ToDo added</h3>}
       {todo.map((item) => {
         return (
-          <div className="flex justify-between w-full px-4" key={item.id}>
-            <p className="inline-block w-full text-left border-2 border-black p-2">
+          <div className="flex justify-between w-full px-4 " key={item.id}>
+            <p className=" overflow-hidden truncate inline-block w-full text-left max-w-[318px] border-2 border-black p-2">
               {item.title}
             </p>{" "}
-            <button
-              className="text-white bg-black shadow border border-r-[#525151] border-black hover:scale-110 hover:duration-300   px-2 text-2xl flex justify-center items-center"
-              onClick={() => dispatch(deleteTodo(item.id))}
-            >
-              <BiEdit />
-            </button>
             <button
               className="text-white bg-black shadow border border-black hover:scale-110 hover:duration-300   px-2 text-2xl flex justify-center items-center"
               onClick={() => dispatch(deleteTodo(item.id))}
@@ -40,16 +41,18 @@ export const Todos = () => {
           </div>
         );
       })}
-      {!empty && (
-        <div className="flex justify-center">
-          <button
-            onClick={() => dispatch(removeTodo())}
-            className="bg-black text-white p-2 hover:scale-110 hover:duration-300"
-          >
-            Remove All
-          </button>
-        </div>
-      )}
+
     </div>
+          {!empty && (
+            <div className="flex justify-center mt-4">
+              <button
+                onClick={() => dispatch(removeTodo())}
+                className="bg-black text-white p-2 hover:scale-110 hover:duration-300"
+              >
+                Remove All
+              </button>
+            </div>
+          )}
+          </>
   );
 };
